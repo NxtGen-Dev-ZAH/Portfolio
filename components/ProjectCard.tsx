037637f8a932df538eec35e471c3103e88755aff
NxtGen-Dev-ZAH <zaheer.ahmad.contact@gmail.com>
2024-05-10 01:31:38 +0500

    second

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -1,4 +1,3 @@
-"use client";
 import React, { useState } from "react";
 import { motion } from "framer-motion";
 
@@ -18,39 +17,43 @@ const ProjectCard = ({ image, title, text }: Props) => {
       setIsAnimating(true);
     }
   }
+
   return (
-    <div
-      onClick={handleFlip}
-      className="w-[450px] h-[280px] rounded-md cursor-pointer"
+    <motion.div
+      className="w-[450px] h-[280px] rounded-md cursor-pointer overflow-hidden"
+      whileHover={{ scale: 1.1 }}
+
     >
-      <motion.div
-        className="flip-card-inner w-full h-full"
-        initial={false}
-        animate={{ rotateY: isFlipped ? 180 : 360 }}
-        transition={{ duration: 0.6, animationDirection: "normal" }}
-        onAnimationComplete={() => setIsAnimating(false)}
-      >
-        <div
-          style={{ backgroundImage: `url(${image})` }}
-          className="w-full h-full group relative flip-card-front bg-cover bg-center text-white rounded-lg p-4"
+      <div onClick={handleFlip} className="w-full h-full relative">
+        <motion.div
+          className="flip-card-inner w-full h-full"
+          initial={false}
+          animate={{ rotateY: isFlipped ? 180 : 360 }}
+          transition={{ duration: 0.6, animationDirection: "normal" }}
+          onAnimationComplete={() => setIsAnimating(false)}
         >
-          <div className="absolute inset-0 w-full h-full rounded-md bg-black opacity-0 group-hover:opacity-40" />
-          <div className="absolute inset-0 w-full h-full text-[30px] font-bold pb-10 hidden group-hover:flex items-center z-[20] justify-center ">
-            Learn more &gt;
+          <div
+            style={{ backgroundImage: `url(${image})` }}
+            className="w-full h-full group relative flip-card-front bg-cover bg-center text-white rounded-lg p-4"
+          >
+            <div className="absolute inset-0 w-full h-full rounded-md bg-black opacity-0 group-hover:opacity-40" />
+            <div className="absolute inset-0 w-full h-full text-[30px] font-bold pb-10 hidden group-hover:flex items-center z-[20] justify-end ">
+              Learn more &gt;
+            </div>
           </div>
-        </div>
-        <div
-          style={{ backgroundImage: `url(${image})` }}
-          className="w-full h-full group relative flip-card-back bg-cover bg-center text-white rounded-lg p-4"
-        >
-          <div className="absolute inset-0 w-full h-full rounded-md bg-black opacity-50 z-[-1]" />
-          <div className="flex flex-col gap-16 py-3 z-[30]">
-            <h1 className="text-white text-2xl font-semibold">{title}</h1>
-            <p className="text-gray-200 text-[20px] font-medium">{text}</p>
+          <div
+            style={{ backgroundImage: `url(${image})` }}
+            className="w-full h-full group relative flip-card-back bg-cover bg-center text-white rounded-lg p-4"
+          >
+            <div className="absolute inset-0 w-full h-full rounded-md bg-black opacity-50 z-[-1]" />
+            <div className="flex flex-col gap-16 py-3 z-[30]">
+              <h1 className="text-white text-2xl font-semibold">{title}</h1>
+              <p className="text-gray-200 text-[20px] font-medium">{text}</p>
+            </div>
           </div>
-        </div>
-      </motion.div>
-    </div>
+        </motion.div>
+      </div>
+    </motion.div>
   );
 };
 
