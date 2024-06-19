b28f9dfda1d5f916344ea47ab16b31e3c5111820
NxtGen-Dev-ZAH <zaheer.ahmad.contact@gmail.com>
2024-06-19 12:16:56 +0500

    import updated

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
 "use client";
-import Home from "@/components/Home";
-import Navbar from "@/components/Navbar";
-import Services from "@/components/Services";
-import Footer from "@/components/footer";
-import Contact from "@/components/Contact";
-import Project from "@/components/project";
-import Skill from "@/components/skill";
+import Home from "../components/Home";
+import Navbar from "../components/Navbar";
+import Services from "../components/Services";
+import Footer from "../components/footer";
+import Contact from "../components/Contact";
+import Project from "../components/project";
+import Skill from "../components/skill";
 import Image from "next/image";
 
 export default function Main() {
